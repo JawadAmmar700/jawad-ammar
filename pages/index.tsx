db602f4c69115edde6cc101da67e7231a0e1c138
jawadGitNext <jawadGitNext2021@gmail.com>
2022-03-13 15:28:54 +0200

    ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,14 +56,9 @@ export default function Home({ data }: { data: string }) {
         <title>Jawad's resume</title>
         <link rel="icon" href="/J.png" />
       </Head>
-      {/* <div className="w-full h-screen fixed">
+      <div className="w-full h-screen fixed">
         <Image src="/fixedImage.jpg" alt="fixed image" layout="fill" />
-      </div> */}
-      <img
-        src="/fixedImage.jpg"
-        alt="fixed image"
-        className="w-full h-screen fixed z-0"
-      />
+      </div>
       <main className="w-full absolute top-0">
         <Header refs={refs} />
         <div>
